00c64152e6b04868fb062286285c40365fcb684d
root <root@yk.example.com>
2025-08-04 19:49:13 +0530

    new files added

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Node.js app via Docker & CI/CD deployed by Chitransh Jangid");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://192.168.1.18:${PORT}`);
+});
+
